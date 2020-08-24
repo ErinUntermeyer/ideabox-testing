948e008ec550c5c9e7030bbd1f2ebc751d04a656
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-24 10:29:16 -0600

    Add componentDidMount method to App

--- src/App/App.js
+++ src/App/App.js
@@ -9,7 +9,13 @@ export default class App extends Component {
     this.state = {
       ideas: []
     };
-  }
+	}
+	
+	componentDidMount() {
+		fetch('http://localhost:3001/api/v1/ideas')
+			.then(response => response.json())
+			.then(data => this.setState({ ideas: data }))
+	}
 
   addIdea = newIdea => {
     this.setState({ ideas: [...this.state.ideas, newIdea]})

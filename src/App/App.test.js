6580e2d1c2467dc11acc288a810875a99183cc76
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-24 09:04:08 -0600

    Add first test for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+	it('should add a card to the DOM when the form is filled out', () => {
+		// set up - render the app
+		render(<App />)
 
+		// execution - fill out the form component, submit the form, find the resulting card
+		const titleInput = screen.getByPlaceholderText('title')
+		const descriptionInput = screen.getByPlaceholderText('description')
+		const submitButton = screen.getByRole('button', {name:'Submit!'})
+
+		fireEvent.change(titleInput, {target: { value: 'New Title' }})
+		fireEvent.change(descriptionInput, {target: { value: 'New Description' }})
+
+		fireEvent.click(submitButton)
+
+		const newTitle = screen.getByRole('heading', {name: 'New Title'})
+		const newDescription = screen.getByText('New Description')
+
+		// assertion
+		expect(newTitle).toBeInTheDocument()
+		expect(newDescription).toBeInTheDocument()
+
+	})
+
+})
 
 // test here that input fields get cleared upon clicking submit
 // test here that you cannot have empty inputs 
